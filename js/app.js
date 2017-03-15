5379ce16c508d04924c1bc707c76fb7025f1eda7
Jessica <11lopez.jessica@gmail.com>
2017-03-15 09:12:04 -1000

    JSON

--- js/app.js
+++ js/app.js
@@ -32,48 +32,50 @@ var simplifiedJSON = [
   "journalEntry" : "1".
   "date" : " August 15, 2016", 
   "title" : "Purpose",
-  "content" : "I wanted to take this class because I thought it was interseting to find out how to code. I had an interseting in coding when I did code.org and scratch. I thought it was cool to keep trying. I want to be able to able to code for FRC in Robotics because we use JavaScript. I think it will also help me create my own website the harder way compared to taking the easy way out and go on a website already created and make a new website.", 
+  "content" : "Why I took this class and what I want to get out of it",
+  "article" : "I wanted to take this class because I thought it was interseting to find out how to code. I had an interseting in coding when I did code.org and scratch. I thought it was cool to keep trying. I want to be able to able to code for FRC in Robotics because we use JavaScript. I think it will also help me create my own website the harder way compared to taking the easy way out and go on a website already created and make a new website.", 
   "img" : undefined
  }, 
  {
   "journalEntry" : "2",
   "date" : "August 24, 2016",
   "title" : "Different Between UX and UI",
-  "content" : "UX stands for User Experience. This is the experience people get when they entire a website. UI stands for User Interface. User Interface is suppose to be eye appealing for the user."
+  "content" : "Difference between UX and UI",
+  "article" : "UX stands for User Experience. This is the experience people get when they entire a website. UI stands for User Interface. User Interface is suppose to be eye appealing for the user."
  },
   {
     "journalEntry" : "3",
     "date" : "September 2, 2016",
     "title" : "HTTP and CSS",
-    "content" :  
+    "content" :  "HTTP and CSS",
     "article" : "HTTP is the language that describes the structure of Web pages like the headings, tables, photos, etc. CSS is the style sheet which includes colors, layouts and fonts."
   },
   {
     "journalEntry" : "4", 
     "date" : "September 26, 2016",
     "title" : "Responsive Design", 
-    "content" : 
+    "content" : "What is a Responsive Web Design",
     "article" : "A Responsive Web Design is sites that can fit to all types of sizes. If I am making the screen smaller or bigger it's able to adjust to the screen size so I can see all the content. For example Facebook allows you see all the content if you have the screen at a regular size or if you make it smaller."
   }, 
   {
     "journalEntry"  : "5", 
     "date" : "September 28, 2016", 
     "title" : "Digimon Go",
-    "content" : 
+    "content" : "Reflection on DigimonGo",
     "article" : "Doing a clone on Digimon Go was very interesting. It was surprising when I was able to create a website that was replicated to another one. It was difficult at first trying to figure out what I wanted to start on. I wanted to go from top to bottom making sure I had my layout right, but I decided to put everything on the screen and work from there. It was hard to do the background image and add the div block right next to it, but it got easier when I had some help with it. I was very proud with myself how I was able to finish everything on time. This was a great assignment I was able to do"
   },
   {
   "journalEntry" : "6", 
   "date" : "October 7, 2016", 
   "title" : "CSS Media Queries",
-  "content" : 
+  "content" : "Reflection on Media Queries",
   "article" : "CSS Media Queries are used to make the website fit to your screen. If you're using a computer it will fit to your screen, if you're using a phone you'll be able to see the website just like on the computer. I think it's very useful to use so that you'll be able to see your website from many devices. When doing the Block family it was very fun being able to minimize and maximize my screen and change the colors for the screen. It was difficult at first to start doing this because you need really specific commands you want for the website to do and if you do it wrong it can mess up your whole entire design."
   },
   {
     "journalEntry" : "7",
     "date" : "October 31, 2016",
     "title" : "Basic JavaScript",
-    "content" : 
+    "content" : "Reflection on Basic JavaScript",
     "article" : "At first during the break I started to learn JavaScript and it looked so confusing. It seemed a little easier than html because you just placed var in front to name a variable type which was kind of an easier concept, but you had to name the different data types that you had to remember which is confusing to do. During the school day, when we came back, I was getting it. I now know the different data types-Boolean, String, Array, Object, Number, etc. I'm still getting the hang of it, but I think it's more efficient than html."
   },
   {
@@ -82,5 +84,6 @@ var simplifiedJSON = [
     "title" : "Conditionals",
     "content" : "Reflections on Aliens",
     "article" : "I really liked doing Aliens. It helped me kind of understand a little more how JavaScript works. We had to test it in jsbin and it didn't work at first, but then me and my partner realized it was because one statement wasn't true so it wouldn't have worked. It was kind of difficult with my partner. I don't know my partner as well so it was weird to interact with him. It was like he was doing most of the work which was kind of hard to understand what I should be doing. I know how to work with people now, but it was really good to do."
-  }
+  },
+  
 ]
